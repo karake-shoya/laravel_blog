8c8c9487e0c702cfc44ff4cd149181719f37640b
naiawa <karake.shoya@gmail.com>
2023-12-11 20:14:03 +0900

    自分の記事だけ削除ボタンが表示される

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -12,11 +12,13 @@
     <h1 class="h1">{{ $article->title}}</h1>
     <div class="h5 pt-2 pb-3 border-bottom">{{ $article->created_at}}</div>
     <div class="h3 pt-4">{!! nl2br(e($article->body))!!}</div>
+    @if($article && Auth::check() && Auth::user()->id == $article->user_id)
     <form action="{{ route('articles.destroy', $article) }}" method="post">
         @csrf
         @method('delete')
         <button type="submit" class="btn btn-outline-secondary text-danger">削除する</button>
     </form>
+    @endif
 </article>
 
 </div>
