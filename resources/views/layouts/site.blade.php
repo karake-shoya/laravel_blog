2cbfda6f7e0f8df39fc0fc529ada36e7f9e04ecf
naiawa <karake.shoya@gmail.com>
2024-01-30 20:04:05 +0900

    ハンバーガーメニューの実装

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -15,37 +15,60 @@
 
 <body class="container-fluid">
 
-    <div id="header" class="sticky-top d-flex flex-md-row align-items-center p-3 px-md-4 mb-3 border-bottom shadow-sm bg-light">
-        <a class="my-0 mr-md-auto" href="/home">
-            <img src="{{ asset('storage/blog_images/logo.png') }}" class="logo-image" alt="logo" style="width:50px; height:auto;">
-        </a>
-        <nav class="my-2 my-md-0 mr-md-3 d-flex ms-auto">
-            @auth
-                <a class="btn btn-outline-success me-2" href="/post">投稿</a>
-                <a class="btn btn-outline-primary me-2" href="/article">マイページ</a>
-                <div class="dropdown">
-                    <a class="btn btn-outline-dark me-2 dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        {{ Auth::user()->name }}
-                    </a>
-                    <ul class="dropdown-menu">
-                        <x-dropdown-link :href="route('profile.edit')">
-                            {{ __('Profile') }}
-                        </x-dropdown-link>
-                        <form method="POST" action="{{ route('logout') }}">
-                            @csrf
-
-                            <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
-                                                this.closest('form').submit();">
-                                {{ __('Log Out') }}
-                            </x-dropdown-link>
-                        </form>
-                    </ul>
-                </div>
-            @else
-                <a class="btn btn-outline-dark me-2" href="/login">ログイン</a>
-            @endauth
+    <div id="header" class="sticky-top bg-light">
+        <!-- ナビゲーションバーの開始 -->
+        <nav class="navbar navbar-expand-md navbar-light">
+            <a class="navbar-brand" href="/home">
+                <img src="{{ asset('storage/blog_images/logo.png') }}" class="logo-image" alt="logo" style="width:50px; height:auto;">
+            </a>
+            <!-- ハンバーガーメニュートグルの追加 (中サイズ以下で表示) -->
+            <button class="navbar-toggler d-md-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <!-- ナビゲーションバーのコンテンツ -->
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    @auth
+                        <!-- 中サイズ以上でボタンとして表示 -->
+                        <li class="nav-item d-none d-md-inline-block">
+                            <a class="btn btn-outline-success me-2" href="/post">投稿</a>
+                        </li>
+                        <li class="nav-item d-none d-md-inline-block">
+                            <a class="btn btn-outline-primary me-2" href="/article">マイページ</a>
+                        </li>
+                        <!-- 中サイズ以下でハンバーガーメニュー内に表示 -->
+                        <li class="nav-item d-md-none">
+                            <a class="nav-link" href="/post">投稿</a>
+                        </li>
+                        <li class="nav-item d-md-none">
+                            <a class="nav-link" href="/article">マイページ</a>
+                        </li>
+                        <!-- ドロップダウンメニュー (全サイズで表示) -->
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                {{ Auth::user()->name }}
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuLink">
+                                <a class="dropdown-item" href="{{ route('profile.edit') }}">{{ __('Profile') }}</a>
+                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); this.closest('form').submit();">{{ __('Log Out') }}</a>
+                                <form method="POST" action="{{ route('logout') }}" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                    @else
+                        <li class="nav-item d-none d-md-inline-block">
+                            <a class="btn btn-outline-dark me-2" href="/login">ログイン</a>
+                        </li>
+                        <!-- 中サイズ以下でハンバーガーメニュー内に表示 -->
+                        <li class="nav-item d-md-none">
+                            <a class="nav-link" href="/login">ログイン</a>
+                        </li>
+                    @endauth
+                </ul>
+            </div>
         </nav>
+        <!-- ナビゲーションバーの終了 -->
     </div>
 
     <div id="main" class="pt-2 pb-5">
