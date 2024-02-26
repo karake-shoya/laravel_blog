46c683eb7f2d4573d4931b945795aa82e97ad76f
naiawa <karake.shoya@gmail.com>
2024-02-26 13:49:39 +0900

    npm再インストール

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
+                'resources/css/main.css',
                 'resources/sass/app.scss',
                 'resources/js/app.js',
             ],
